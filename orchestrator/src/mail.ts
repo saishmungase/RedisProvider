3b6a155f2f8a7833accfb8d39db5d390ab246427
Saish Mungase <saishmungase@gmail.com>
2026-01-24 19:35:00 +0530

    [CORE] business-layer-mail :: setting up the mailing module -> to efficient mail management using fastforwardit

---
+++ orchestrator/src/mail.ts
@@ -0,0 +1,22 @@
+import { mailer } from "./libs.js"
+import { v4 } from "uuid";
+import { redis } from "./utils.js";
+
+export const mailVarification = async (email : string) => {
+
+    const id = v4();
+    
+    try {
+        await mailer.sendBasic({
+            to: email,
+            subject: "Verification Code!",
+            text: `$Hey Your Verficiation Code is ${id}`
+        });
+
+        await redis.hset("auth", { email :  id } );
+    } catch (error) {
+        console.log("Error While Sending Mail");
+        return { status : "error-mail", message : "Error while sending Mail" }
+    }
+    return { status : "success-mail", code : id, message : "Mail Shared Successfully"}
+}
