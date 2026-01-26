f06e3826f68d9ede889128a7a49fa94433870612
Saish Mungase <saishmungase@gmail.com>
2026-01-27 00:25:43 +0530

    [CORE] business-layer-db :: auth & instance query implementation -> for retrive and update data

---
+++ redis-back/business-layer/src/db/queries.ts
@@ -0,0 +1,18 @@
+export const signUpQuery = `
+  INSERT INTO users (email, password, "firstName", "lastName") 
+  VALUES ($1, $2, $3, $4) 
+  RETURNING id, email;
+`;
+
+export const isUserExist  = `SELECT id, email FROM users WHERE id = $1`
+
+export const loginQuery = `SELECT id, password, "firstName" FROM users WHERE email = $1`
+
+export const createInstanceQuery = `
+  INSERT INTO instances (containerId, port, password, instanceUSER) VALUES ($1, $2, $3, $4)
+  RETURNING id, port, password, instanceUSER;
+`
+
+export const isUserExistByEmail = `
+  SELECT id FROM users WHERE email = $1
+`
