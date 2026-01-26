2f2a81bd83e0d058eb7997364af0570e0544b860
Saish Mungase <saishmungase@gmail.com>
2026-01-27 00:26:56 +0530

    [CORE] business-layer-api :: complete auth flow with JWT, bcrypt, and instance creation -> for better user management

--- redis-back/business-layer/src/api.ts
+++ redis-back/business-layer/src/api.ts
@@ -4,6 +4,11 @@ import { mailVarification } from './mail.js';
 import { createInstance } from "@redis/service/root/manager.js"
 import { validateCode } from './db.js';
 import type { Request, Response, NextFunction } from 'express';
+import pool from './db/index.js';
+import bcrypt from "bcrypt";
+import type { JwtPayload } from 'jsonwebtoken'
+import jwt from 'jsonwebtoken'
+import { createInstanceQuery, isUserExist, isUserExistByEmail, loginQuery, signUpQuery } from './db/queries.js';
 
 export const app = express();
 
@@ -12,8 +17,8 @@ const mailSchema = z.object({
 });
 
 const signUpSchema = z.object({
-  name: z.string(),
-  age: z.number(),
+  firstName: z.string(),
+  lastName: z.string(),
   email: z.string().email(),
   password: z.string().min(8),
   passcode: z.string()
@@ -23,6 +28,19 @@ const signInSchema = z.object({
   email : z.string().email(),
   password : z.string()
 })
+
+const saltRounds : number = Number(process.env.SHIFT)
+const secret = process.env.SECRET
+
+if (!secret) {
+  throw new Error("JWT_SECRET not defined");
+}
+
+if (!saltRounds || isNaN(saltRounds)) {
+  throw new Error("Invalid bcrypt salt rounds");
+}
+
+
 
 app.use(express.json());
 
@@ -39,10 +57,49 @@ app.use(express.json());
 
 */
 
-const verifyToken = (req : Request, res : Response, next : NextFunction) => {
-  const {token} = req.body;
-  console.log("Verfying Token....." + token)
-  next();
+interface AuthPayload extends JwtPayload {
+  userId: string;
+  email: string;
+  firstName: string;
+}
+
+const verifyToken = async (req : Request, res : Response, next : NextFunction) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader)
+    return res.status(401).send({ message: "No token provided" });
+
+  const token = authHeader.split(" ")[1];
+  if(!token){
+    return res.status(401).send({ message: "No token provided" });
+  }
+  try {
+    const decoded = jwt.verify(
+      token,
+      secret
+    ) as AuthPayload;
+
+    const result = await pool.query(
+      isUserExist,
+      [decoded.userId]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(401).send({
+        message: "Invalid email or password"
+      });
+    }
+
+    req.user = {
+      userId: decoded.userId,
+      email: decoded.email,
+      name: decoded.firstName
+    };
+
+    next();
+  } catch (err) {
+    return res.status(401).send({ message: "Invalid token" });
+  }
 }
 
 app.post("/signup", async (req, res) => {
@@ -50,95 +107,165 @@ app.post("/signup", async (req, res) => {
     const parseResult = mailSchema.safeParse(data);
 
     if(!parseResult.success){
-      res.send({
-        status : 400,
+      res.status(400).send({
         message : "Invalid Credentials"
       });
       return;
     }
     
     const {email} = data;
+    const existing = await pool.query(isUserExistByEmail, [email]);
+
+    if (existing.rows.length > 0) {
+      return res.status(409).send({
+        message: "User already exists"
+      });
+    }
+
     const mailState = await mailVarification(email);
 
     if(mailState.status == "error-mail"){
-      return res.send({
-        status : 400,
+      return res.status(400).send({
         message : "Error While Sending Code, Please Try Later or use different mail id"
       })
     }
 
-    return res.send({
-      status : 200,
+    return res.status(200).send({
       message : "Code is Being Shared To Your Mail Id Please Check!"
     })
     
 })
 
-app.post("/varified-signup", async (req, res) => {
+app.post("/verified-signup", async (req, res) => {
 
   const data = req.body; 
 
   const parseResult = signUpSchema.safeParse(data);
 
   if(!parseResult.success){
-    res.send({
-      status : 400,
+    res.status(400).send({
       message : "Invalid Credentials"
     });
     return;
   }
 
-  const {name, age, email, password, passcode} = data;
+  const {firstName, lastName, email, password, passcode, } = data;
 
   const isValid = await validateCode({ code: passcode, email });
   if(!isValid){
     return res.status(400).send({ message: "Invalid PassCode" });
   }
 
-  console.log("DatBase will be integrated soon, For right now Data Saved Successfully")
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-  res.send({
-    status : 200,
-    token : 99,
-    message : "Data is being Stored Successfully!"
-  })
+  try {
+    const result = await pool.query(signUpQuery, [
+      email,
+      hashedPassword,
+      firstName,
+      lastName
+    ]);
+
+    const user = result.rows[0];
+
+    const token = jwt.sign(
+      {
+        userId: user.id,
+        email: user.email,
+        firstName : user.firstName
+      },
+      secret,
+      { expiresIn: "7d" }
+    );
+
+    return res.status(200).send({
+      token : token,
+      message : "Data is being Stored Successfully!"
+    })
+  } catch (error) {
+    console.log(error)
+    return res.status(500).send({
+      message : "Unable to Store User To Database!"
+    })
+  }
 })
 
-app.post("/login", (req, res) => {
+app.post("/login", async (req, res) => {
   const data = req.body;
   const parseResult = signInSchema.safeParse(data);
+  if (!parseResult.success) {
+    return res.status(400).send({
+      message: "Invalid credentials"
+    });
+  }
 
-  const { email, password } = data;
 
-  console.log("Checking DB......")
+  const { email, password } = parseResult.data;
 
-  res.send({
-    status : 200,
-    token : 99,
+  const result = await pool.query(loginQuery, [email])
+
+  if (result.rows.length === 0) {
+    return res.status(401).send({
+      message: "Invalid email or password"
+    });
+  }
+
+  const hashedPass = result.rows[0].password;
+  const userId = result.rows[0].id;
+
+  const verfication = await bcrypt.compare(password, hashedPass);
+
+  if (!verfication) {
+    return res.status(401).send({
+      message: "Invalid email or password"
+    });
+  }
+
+  const token = jwt.sign(
+    {
+      userId: userId,
+      email: email, 
+      firstName : result.rows[0].firstName
+    },
+    secret,
+    { expiresIn: "7d" }
+  );
+
+
+  res.status(200).send({
+    token : token,
     message : "It's Good To See You Again!"
   })
 })
 
 
 app.post("/createInstance", verifyToken, async (req, res) => {
-  const userData = req.body
-  const { id, email, name } = userData;
+  const { userId, name } = req.user!;
 
   try {
 
-    const { userId, containerId, port, password } = await createInstance({ userId : id, userName : name })
+    const { status, containerId, assignedPort, redisPassword } = await createInstance({ userId, userName : name })
 
-    console.log("Saving User Data To the Database")
+    if(status != 200){
+      return res.status(status).send({
+        message : "Error While Creating Instance"
+      })
+    }
+
+    const saving = await pool.query(createInstanceQuery, [containerId, assignedPort, redisPassword, userId]);
+
+    const { id, port, password, instanceUSER } = saving.rows[0]
     
-    return res.send({
-      status : 200,
+    return res.status(200).send({
+      data : {
+        id, port, password, instanceUSER
+      },
       message : "Redis Instance Has Been Saved Successfully!"
     })
 
   } catch (error) {
     console.log( "Erro While Creating a Job:- " + error)
-    return res.send({
-      status : 500,
+    return res.status(500).send({
       message : "Internal Server Error !"
     })
   }
