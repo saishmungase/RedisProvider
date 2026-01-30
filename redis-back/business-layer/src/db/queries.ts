ddd503241c8ce1567e6ad176f287c851c15eabf0
Saish Mungase <saishmungase@gmail.com>
2026-01-30 19:21:17 +0530

    [CORE] query :: query implementations for instances -> to fetch active and user instances

--- redis-back/business-layer/src/db/queries.ts
+++ redis-back/business-layer/src/db/queries.ts
@@ -1,12 +1,12 @@
 export const signUpQuery = `
-  INSERT INTO users (email, password, "firstName", "lastName") 
+  INSERT INTO users (email, password, firstname, lastname) 
   VALUES ($1, $2, $3, $4) 
   RETURNING id, email;
 `;
 
 export const isUserExist  = `SELECT id, email FROM users WHERE id = $1`
 
-export const loginQuery = `SELECT id, password, "firstName" FROM users WHERE email = $1`
+export const loginQuery = `SELECT id, password, firstname FROM users WHERE email = $1`
 
 export const createInstanceQuery = `
   INSERT INTO instances (containerId, port, password, instanceUSER) VALUES ($1, $2, $3, $4)
@@ -15,4 +15,12 @@ export const createInstanceQuery = `
 
 export const isUserExistByEmail = `
   SELECT id FROM users WHERE email = $1
+`
+
+export const fetchInstances = `
+SELECT * FROM instances WHERE instanceUSER = $1;
+`
+
+export const fetchActives = `
+SELECT createdat, port FROM instances WHERE status = 'RUNNING';
 `
