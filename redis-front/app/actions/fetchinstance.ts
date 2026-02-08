6c673ec8c924d0e4d62f6f9d9946849800c9324f
Saish Mungase <saishmungase@gmail.com>
2026-02-08 15:30:03 +0530

    [EDGE] types & status passing :: setting server response types & passing status -> for ts & auto redirects

--- redis-front/app/actions/fetchinstance.ts
+++ redis-front/app/actions/fetchinstance.ts
@@ -1,5 +1,25 @@
 'use server'
 
+export interface RedisMetrics {
+    used_memory: string;
+    used_memory_rss: string;
+    used_memory_peak: string;
+    used_memory_peak_perc: string;
+    total_system_memory: string;
+    maxmemory: string;
+    maxmemory_policy: string;
+    mem_fragmentation_ratio: number;
+    remain_memory: string;
+}
+
+export interface InstanceResponse {
+    username: string;
+    password: string;
+    status: string;
+    createdat: string;
+    data: RedisMetrics;
+}
+
 const fetchInstance = async (port: number, token: string) => {
     try {
         const response = await fetch(`http://localhost:3000/get-instance?port=${port}`, {
@@ -9,9 +29,10 @@ const fetchInstance = async (port: number, token: string) => {
                 "authorization" : token 
             }
         });
-
+        const status = response.status;
         const data = await response.json();
-        return data;
+        data["status"] = status;
+        return data
 
     } catch (error) {
         console.error("Server Action Error:", error);
