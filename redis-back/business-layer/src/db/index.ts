3bc49f9d88c0f31c5a1fbba2596dedc8238574e9
Saish Mungase <saishmungase@gmail.com>
2026-02-15 18:08:12 +0530

    [EDGE] security :: ssl setup for unauthorized request -> improved security

--- redis-back/business-layer/src/db/index.ts
+++ redis-back/business-layer/src/db/index.ts
@@ -5,9 +5,7 @@ const url = process.env.DATABASE_URL
 
 const pool = new Pool({
   connectionString: url,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  ssl: { rejectUnauthorized: false },
 });
 
 export default pool;
