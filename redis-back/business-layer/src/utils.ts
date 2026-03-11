ae6e48821d171cee4a06db01c5cdbbb8da8d92f1
Saish Mungase <saishmungase@gmail.com>
2026-03-11 23:32:08 +0530

    [LABS] utils :: inititalization of bloom filter -> to start the bloom filter with server initialization

--- redis-back/business-layer/src/utils.ts
+++ redis-back/business-layer/src/utils.ts
@@ -3,15 +3,23 @@ import 'dotenv/config';
 import { app } from "./api.js";
 import { Redis } from '@upstash/redis'
 import { cleanup } from "./cleaner.js";
+import { BloomFilter } from './helper/bloom.js';
+import { initBloomFilter } from './initBloom.js';
 
 const port = process.env.PORT;  
 const url = process.env.REDIS_REST_URL
 const token = process.env.REDIS_REST_TOKEN
+const bucketSize = process.env.BUCKET_SIZE || 100000
+const shift = process.env.SHIFT || 9
+
+export const bloomFilter = new BloomFilter(Number(bucketSize), Number(shift));
 
 export const redis = new Redis({
   url,
   token,
 })
+
+initBloomFilter();
 
 cleanup()
 
